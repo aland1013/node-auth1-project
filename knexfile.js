007c119f42856d8bf98e2c0abde31870222ef1fc
aland1013 <adam.land@gmail.com>
2020-08-04 23:51:54 -0400

    created knexfile and create-table migration

---
+++ knexfile.js
@@ -0,0 +1,19 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsTrue: true,
+    connection: {
+      filename: './data/users.db3'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    },
+    migrations: {
+      directory: './data/migrations'
+    }
+  }
+};
