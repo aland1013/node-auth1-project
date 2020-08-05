7f79adae69794904b1d5331a0f6c0a7fbe76601a
aland1013 <adam.land@gmail.com>
2020-08-05 00:02:26 -0400

    created GET /api/users endpoint without authentication

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const Users = require('../users/users-model');
+
 const server = express();
 
 server.use(helmet());
@@ -10,6 +12,15 @@ server.use(cors());
 
 server.get('/', (req, res) => {
   res.json({ api: 'up' });
+});
+
+/* ----- GET /api/users ---- */
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => res.send(err));
 });
 
 module.exports = server;
